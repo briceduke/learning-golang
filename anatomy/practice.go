8f306ac389f1549140ee2f1d7fa8dd707e87efa8
Brice Duke <bricewduke@gmail.com>
2021-04-03 15:42:54 -0500

    ✨ Learn basic anatomy of a go file

---
+++ anatomy/practice.go
@@ -0,0 +1,12 @@
+// Package name, every program needs a package
+package main
+
+// Imports libraries
+import "fmt"
+
+// Main function, every program needs package main and main function.
+// Can only have one main function per directory
+// go run practice.go
+func main()  {
+	fmt.Println("Hello World!")
+}

5bc090f3b426633987516d8601ff13aa8dafc003
briceduke <bricewduke@gmail.com>
2021-04-05 15:20:04 -0500

    ❓ If Statements

---
+++ statements/if.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+
+
+func main() {
+	var someVar = 9
+
+	if someVar < 10 {
+		fmt.Println(someVar)
+	} else if someVar > 10 {
+		// Logic
+	} else {
+		// Logic
+	}	
+
+	// err := handleError()
+
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// }
+
+	// if err := handleError(); err != nil {
+	// 	fmt.Println(err.Error())
+	// }
+}
